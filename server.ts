ecc2757682592039480e289350d294ef2566aa21
faizanalibaig <riazfiaz6181@gmail.com>
2025-09-16 16:30:19 +0500

    husky setup complete

--- server.ts
+++ server.ts
@@ -1,15 +1,11 @@
 require("dotenv").config();
 
-import http from "http";
-import express, { Request, Response } from "express";
-
-// npm i --save-dev @types/express
+const http = require("http");
+const express = require("express");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 const server = http.createServer(app);
-
-app.get("/", (req: Request, res: Response) => {});
 
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);

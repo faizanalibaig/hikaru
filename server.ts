adc755acd6f7863f605398339f95cd878574f271
faizanalibaig <riazfiaz6181@gmail.com>
2025-09-24 19:12:35 +0500

    morgan + winston setup for logging requests

--- server.ts
+++ server.ts
@@ -4,6 +4,7 @@ require("module-alias/register");
 const http = require("http");
 import express from "express";
 const mongoose = require("mongoose");
+const morgan = require("morgan");
 
 const logger = require(`@config/logger`);
 const AppRoutes = require(`@routes/route`);
@@ -15,8 +16,6 @@ const server = http.createServer(app);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-logger.info("Logger is configured");
-
 mongoose
   .connect("mongodb://admin:root123$$@mongo:27017/hikaru")
   .then(() => {
@@ -25,6 +24,14 @@ mongoose
   .catch((err: any) => {
     console.error("MongoDB connection error:", err);
   });
+
+app.use(
+  morgan("combined", {
+    stream: {
+      write: (message: string) => logger.info(message.trim()),
+    },
+  })
+);
 
 app.use("/api/v1", AppRoutes);
 
